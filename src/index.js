1ba9173ec771ed5200f910b94af5dcb34a6afc7b
Chase McCoy <chasem000@gmail.com>
2019-01-08 21:31:19 -0600

    v0.4.1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ export { default as Text } from 'components/Text'
 export { default as Heading } from 'components/Heading'
 export { default as Button } from 'components/Button'
 export { UnorderedList } from 'components/Lists'
+export { default as CSSReset } from 'components/CSSReset'
 
 // Utils
 export { default as media } from 'utils/media'
